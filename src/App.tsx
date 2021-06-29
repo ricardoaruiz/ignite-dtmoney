52e00bb0f70ed995fb3387bd7c0e065bb8ddc50a
Ricardo Ruiz <ricardo.ruiz.ext@dasa.com.br>
2021-06-28 21:33:38 -0300

    style: config fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export const App = () => {
   return (
     <div className='App'>
-      <h1>APP 1</h1>
+      <h1>Hello World</h1>
     </div>
   );
 };
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -20,12 +20,27 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   html {
-    font-size: 62.5%;
+    @media (max-width: 1080px) {
+      font-size: 93.75%; //15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%; //14px
+    }
   }
 
   body {
     background-color: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
